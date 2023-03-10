cb17f09e839ed19c84fc30871ceac0138ff39cf2
imob <andrew.narolsky@gmail.com>
2023-03-10 15:29:19 +0200

    changing admin modules

--- html/resources/js/admin.js
+++ html/resources/js/admin.js
@@ -1,35 +1,35 @@
 import 'bootstrap'
 
-import CardRefresh from './admin/CardRefresh'
-import CardWidget from './admin/CardWidget'
+// import CardRefresh from './admin/CardRefresh'
+// import CardWidget from './admin/CardWidget'
 import ControlSidebar from './admin/ControlSidebar'
 import DirectChat from './admin/DirectChat'
-import Dropdown from './admin/Dropdown'
-import ExpandableTable from './admin/ExpandableTable'
+// import Dropdown from './admin/Dropdown'
+// import ExpandableTable from './admin/ExpandableTable'
 import Fullscreen from './admin/Fullscreen'
-import IFrame from './admin/IFrame'
-import Layout from './admin/Layout'
+// import IFrame from './admin/IFrame'
+// import Layout from './admin/Layout'
 import PushMenu from './admin/PushMenu'
 import SidebarSearch from './admin/SidebarSearch'
 import NavbarSearch from './admin/NavbarSearch'
-import Toasts from './admin/Toasts'
-import TodoList from './admin/TodoList'
+// import Toasts from './admin/Toasts'
+// import TodoList from './admin/TodoList'
 import Treeview from './admin/Treeview'
 
 export {
-    CardRefresh,
-    CardWidget,
+    // CardRefresh,
+    // CardWidget,
     ControlSidebar,
-    DirectChat,
-    Dropdown,
-    ExpandableTable,
+    // DirectChat,
+    // Dropdown,
+    // ExpandableTable,
     Fullscreen,
-    IFrame,
-    Layout,
+    // IFrame,
+    // Layout,
     PushMenu,
     SidebarSearch,
     NavbarSearch,
-    Toasts,
-    TodoList,
+    // Toasts,
+    // TodoList,
     Treeview
 }
