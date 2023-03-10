3aa3b291337dd7dcf012825ac5c9b77ee362bfed
imob <andrew.narolsky@gmail.com>
2023-03-10 15:23:52 +0200

    added Messages

--- html/app/Http/Middleware/HandleInertiaRequests.php
+++ html/app/Http/Middleware/HandleInertiaRequests.php
@@ -43,7 +43,8 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'success' => $request->session()->get('success'),
+            'error' => $request->session()->get('error'),
         ]);
     }
 }
