9714e6ca303b93d3d6ef51bc98009401e016f6ae
imob <andrew.narolsky@gmail.com>
2023-03-13 11:22:46 +0200

    fixed pagination

--- html/resources/js/Shared/Pagination.vue
+++ html/resources/js/Shared/Pagination.vue
@@ -1,13 +1,22 @@
 <template>
     <ul v-if="links.length > 3" class="pagination pagination-sm m-0 float-right">
         <li v-for="(link, k) in links" class="page-item"
-            :class="{'active': link.active}" >
-            <Link
-                :href="link.url"
-                :key="k"
-                :disabled="link.url === null"
-                v-html="link.label"
-                class="page-link"
+            :class="{'active': link.active}">
+            <Link v-if="link.label === 'pagination.previous'"
+                  :href="link.url"
+                  v-html="'Попередня'"
+                  class="page-link"
+            />
+            <Link v-else-if="link.label === 'pagination.next'"
+                  :href="link.url"
+                  v-html="'Наступна'"
+                  class="page-link"
+            />
+            <Link v-else
+                  :href="link.url"
+                  :disabled="link.url === null"
+                  v-html="link.label"
+                  class="page-link"
             />
         </li>
     </ul>
